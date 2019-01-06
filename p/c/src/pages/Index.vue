bb8f69fb8481cd48d010930a3c688e34f55938fc
Ligiano Azzi <ldaazzi@gmail.com>
2019-01-06 21:07:21 +0000

    increase html, for show list of clients

--- p/c/src/pages/Index.vue
+++ p/c/src/pages/Index.vue
@@ -1,6 +1,48 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  <q-page>
+    <div class="row">
+    	<div class="col col-md-3"> 
+    		<q-list>
+    			<q-list-header> Leads online </q-list-header>
+
+    			<q-item>
+    				<q-item-side icon="contacts"/>
+    				<q-item-main label="lead 1"/>
+    				<q-item-side right icon="chat_bubble"/>
+    			</q-item>
+
+    			<q-item>
+    				<q-item-side icon="contacts"/>
+    				<q-item-main label="lead 1"/>
+    				<q-item-side right icon="chat_bubble"/>
+    			</q-item>
+
+    			<q-item>
+    				<q-item-side icon="contacts"/>
+    				<q-item-main label="lead 1"/>
+    				<q-item-side right icon="chat_bubble"/>
+    			</q-item>
+
+    			<q-item>
+    				<q-item-side icon="contacts"/>
+    				<q-item-main label="lead 1"/>
+    				<q-item-side right icon="chat_bubble"/>
+    			</q-item>
+
+    			<q-item>
+    				<q-item-side icon="contacts"/>
+    				<q-item-main label="lead 1"/>
+    				<q-item-side right icon="chat_bubble"/>
+    			</q-item>    			    			    			
+    		</q-list>
+    	</div>
+
+    	<div class="col col-md-9">
+    		// chat with the client
+    	</div>
+
+
+    </div>	
   </q-page>
 </template>
 
